a5281f49123a1be78b036254f0989dfb56bce52a
ndemeyvan <ndemeyvan@gmail.com>
2019-12-07 21:42:08 +0100

    page princpal initialise

--- WerayouApp/app/src/main/java/com/example/werayouapp/Activity/ActivityPrincipal.java
+++ WerayouApp/app/src/main/java/com/example/werayouapp/Activity/ActivityPrincipal.java
@@ -3,12 +3,12 @@ package com.example.werayouapp.Activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toolbar;
 
 import com.example.werayouapp.Activity.mainFragment.FriendsFragment;
 import com.example.werayouapp.Activity.mainFragment.HomeFragment;
@@ -24,11 +24,12 @@ public class ActivityPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-//        toolbar=findViewById(R.id.toolbar2);
+        toolbar=findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
         BottomNavigationView navigation =  findViewById(R.id.bottomNavigationView);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         loadFragment(new HomeFragment());
-       // toolbar.setTitle("Home");
+        toolbar.setTitle("Home");
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -39,22 +40,22 @@ public class ActivityPrincipal extends AppCompatActivity {
             Fragment fragment;
             switch (item.getItemId()) {
                 case R.id.home:
-                   // toolbar.setTitle("Home");
+                    toolbar.setTitle("Home");
                     fragment = new HomeFragment();
                     loadFragment(fragment);
                     return true;
                 case R.id.friends:
-                    //toolbar.setTitle("amis");
+                    toolbar.setTitle("amis");
                     fragment = new FriendsFragment();
                     loadFragment(fragment);
                     return true;
                 case R.id.message:
-                   // toolbar.setTitle("messages");
+                    toolbar.setTitle("messages");
                     fragment = new MessageFragment();
                     loadFragment(fragment);
                     return true;
                 case R.id.me:
-                  //  toolbar.setTitle("moi");
+                    toolbar.setTitle("moi");
                     fragment = new MeFragment();
                     loadFragment(fragment);
                     return true;
