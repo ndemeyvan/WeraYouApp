e6bcdb22890e376474f62ac7ed329dff0c0d4749
ndemeyvan <ndemeyvan@gmail.com>
2019-12-08 14:32:32 +0100

    swipe card enable

--- WerayouApp/app/src/main/java/com/example/werayouapp/Activity/mainFragment/HomeFragment.java
+++ WerayouApp/app/src/main/java/com/example/werayouapp/Activity/mainFragment/HomeFragment.java
@@ -1,21 +1,33 @@
 package com.example.werayouapp.Activity.mainFragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.example.werayouapp.R;
+import com.lorentzos.flingswipe.FlingCardListener;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
     View v;
+    private ArrayList<String> al;
+    private ArrayAdapter<String> arrayAdapter;
+    private int i;
+    private SwipeFlingAdapterView flingContainer;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -27,8 +39,69 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         v= inflater.inflate(R.layout.fragment_home2, container, false);
+        al = new ArrayList<>();
+        al.add("php");
+        al.add("c");
+        al.add("python");
+        al.add("java");
+        al.add("html");
+        al.add("c++");
+        al.add("css");
+        al.add("javascript");
+
+        arrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.item, R.id.helloText, al );
+        flingContainer=v.findViewById(R.id.frame);
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                Log.d("LIST", "removed object!");
+                al.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object o) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                makeToast(getActivity(), "left!");
+            }
+
+            @Override
+            public void onRightCardExit(Object o) {
+                makeToast(getActivity(), "Right!");
+
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int i) {
+                // Ask for more data here
+                al.add("XML ".concat(String.valueOf(i)));
+                arrayAdapter.notifyDataSetChanged();
+                Log.d("LIST", "notified");
+                i++;
+            }
+
+            @Override
+            public void onScroll(float v) {
+
+                }
+        });
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                makeToast(getContext(), "Clicked!");
+            }
+        });
 
         return v;
     }
+
+
+    static void makeToast(Context ctx, String s){
+        Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
+    }
+
 
 }
