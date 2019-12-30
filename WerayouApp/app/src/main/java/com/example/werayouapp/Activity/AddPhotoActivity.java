f668f27858109ae35a88fda929d53015be669e42
ndemeyvan <ndemeyvan@gmail.com>
2019-12-30 19:12:51 +0100

    travail surr le setting

--- WerayouApp/app/src/main/java/com/example/werayouapp/Activity/AddPhotoActivity.java
+++ WerayouApp/app/src/main/java/com/example/werayouapp/Activity/AddPhotoActivity.java
@@ -1,16 +1,31 @@
 package com.example.werayouapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.example.werayouapp.R;
 
 public class AddPhotoActivity extends AppCompatActivity {
+    ImageView image;
+    EditText post_description;
+    Button post_button;
+    ProgressBar progressBar;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_photo);
+        image=findViewById(R.id.image);
+        post_description=findViewById(R.id.post_description);
+        post_button=findViewById(R.id.post_button);
+        progressBar=findViewById(R.id.progressBar);
+        toolbar=findViewById(R.id.toolbar);
     }
 }
