b1c7165e7bff8bc76a3519da08bc6b0538261629
ndemeyvan <ndemeyvan@gmail.com>
2019-12-30 18:25:41 +0100

    travail surr le setting

--- WerayouApp/app/src/main/java/com/example/werayouapp/Activity/mainFragment/HomeFragment.java
+++ WerayouApp/app/src/main/java/com/example/werayouapp/Activity/mainFragment/HomeFragment.java
@@ -196,15 +196,17 @@ public class HomeFragment extends Fragment {
     }
 
     public void getUserWant(final String searxhSex){
-       usersDb.addChildEventListener(new ChildEventListener() {
+        usersDb.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                if (dataSnapshot.exists() && !dataSnapshot.child("connections").child("refuser").hasChild(currentUser)&& !dataSnapshot.child("connections").child("accepter").hasChild(currentUser) && dataSnapshot.child("sexe").getValue().toString().equals(searxhSex)) {
-                    Cards item = new Cards(dataSnapshot.child("nom").getValue().toString(),dataSnapshot.child("prenom").getValue().toString(),dataSnapshot.child("image").getValue().toString(),dataSnapshot.child("id").getValue().toString(),dataSnapshot.child("pays").getValue().toString(),dataSnapshot.child("ville").getValue().toString(),dataSnapshot.child("apropos").getValue().toString());
-                    rowsItems.add(item);
-                    messageDeDernierCards.setVisibility(View.GONE);
-                    progressBar.setVisibility(View.INVISIBLE);
-                    arrayAdapter.notifyDataSetChanged();
+                if (dataSnapshot.child("sexe").getValue() != null) {
+                    if (dataSnapshot.exists() && !dataSnapshot.child("connections").child("refuser").hasChild(currentUser) && !dataSnapshot.child("connections").child("accepter").hasChild(currentUser) && dataSnapshot.child("sexe").getValue().toString().equals(searxhSex)) {
+                        Cards item = new Cards(dataSnapshot.child("nom").getValue().toString(),dataSnapshot.child("prenom").getValue().toString(),dataSnapshot.child("image").getValue().toString(),dataSnapshot.child("id").getValue().toString(),dataSnapshot.child("pays").getValue().toString(),dataSnapshot.child("ville").getValue().toString(),dataSnapshot.child("apropos").getValue().toString());
+                        rowsItems.add(item);
+                        messageDeDernierCards.setVisibility(View.GONE);
+                        progressBar.setVisibility(View.INVISIBLE);
+                        arrayAdapter.notifyDataSetChanged();
+                    }
                 }
             }
             @Override
