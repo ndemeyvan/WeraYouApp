58fa31c1a3fd547017c255209d7d659e5ecde300
ndemeyvan <ndemeyvan@gmail.com>
2019-12-31 20:08:39 +0100

    detail

--- WerayouApp/app/src/main/java/com/example/werayouapp/Activity/DetailPhotoActivity.java
+++ WerayouApp/app/src/main/java/com/example/werayouapp/Activity/DetailPhotoActivity.java
@@ -1,15 +1,28 @@
 package com.example.werayouapp.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.werayouapp.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -31,6 +44,11 @@ public class DetailPhotoActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     EditText comment_edittext;
     ImageButton send_comment_button;
+    String nom;
+    DatabaseReference usersDb;
+    ProgressBar progressBar;
+
+
 
 
     @Override
@@ -54,15 +72,151 @@ public class DetailPhotoActivity extends AppCompatActivity {
         recyclerView=findViewById(R.id.recyclerView);
         comment_edittext=findViewById(R.id.comment_edittext);
         send_comment_button=findViewById(R.id.send_comment_button);
-
-
-
-
-
-
+        progressBar=findViewById(R.id.progressBar);
+        // set les extras recuperez
+        description_view.setText(description);
+        Picasso.with(DetailPhotoActivity.this).load(image).into(imageView);
+        date_publication.setText(date);
+        getUserData();
 
 
     }
+
+    public void getUserData(){
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference().child("Users").child(id_user);
+        db.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                usersDb = FirebaseDatabase.getInstance().getReference().child("Users").child(id_user);
+                usersDb.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists() && dataSnapshot.getChildrenCount()>0){
+                            Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                            if(map.get("nom")!=null){
+                                nom = map.get("nom").toString();
+                            }
+                            if(map.get("prenom")!=null){
+                                String prenom = map.get("prenom").toString();
+                                nom_profil.setText(nom +" " + prenom);
+                            }
+                            if(map.get("image")!=null){
+                                String profileImageUrl = map.get("image").toString();
+                                Picasso.with(DetailPhotoActivity.this).load(profileImageUrl).placeholder(R.drawable.ic_launcher_background).into(profil_image);
+                                progressBar.setVisibility(View.INVISIBLE);
+                            }
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+       /* //femme
+        DatabaseReference femmeDb = FirebaseDatabase.getInstance().getReference().child("Users").child("Femme");
+        femmeDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if (dataSnapshot.getKey().equals(user.getUid())){
+                    userSex="Femme";
+                    oppositeUserSex="Homme";
+                    usersDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userSex).child(user.getUid());
+                    usersDb.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.exists() && dataSnapshot.getChildrenCount()>0){
+                                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                                if(map.get("nom")!=null){
+                                    nom = map.get("nom").toString();
+
+                                    makeToast(getActivity(),nom);
+                                }
+                                if(map.get("prenom")!=null){
+                                     prenom = map.get("prenom").toString();
+                                    nomUser.setText(nom +" " + prenom);
+
+                                }
+                                if(map.get("age")!=null){
+                                    userAge = map.get("age").toString();
+                                    age.setText(userAge +" ans");
+                                }
+                                if(map.get("ville")!=null){
+                                    userSex = map.get("ville").toString();
+                                }
+                                if(map.get("image")!=null){
+                                    profileImageUrl = map.get("image").toString();
+                                    Picasso.with(getActivity()).load(profileImageUrl).placeholder(R.drawable.logo).into(cardView2);
+
+                                }
+                                //
+                                if(map.get("recherche")!=null){
+                                    String recherche = map.get("recherche").toString();
+                                    cherche.setText(recherche);
+
+                                }
+                                if(map.get("sexe")!=null){
+                                    String userSexe = map.get("sexe").toString();
+                                    sexe.setText(userSexe);
+
+                                }
+                                //
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });*/
+    }
+
 
     @Override
     public void onBackPressed() {
