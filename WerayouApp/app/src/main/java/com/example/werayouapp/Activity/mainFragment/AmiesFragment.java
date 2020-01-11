c43fefd2595a1192a5b2fa6210868b7dfda97f2b
ndemeyvan <ndemeyvan@gmail.com>
2020-01-11 20:49:44 +0100

    travail sur la liste d'amis et le nombre

--- WerayouApp/app/src/main/java/com/example/werayouapp/Activity/mainFragment/AmiesFragment.java
+++ WerayouApp/app/src/main/java/com/example/werayouapp/Activity/mainFragment/AmiesFragment.java
@@ -26,7 +26,9 @@ public class AmiesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_amies, container, false);
+        View v= inflater.inflate(R.layout.fragment_amies, container, false);
+
+        return v;
     }
 
 }
