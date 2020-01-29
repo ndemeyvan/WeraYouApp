2ac7302584cb853a86cea83dd142b9a329944d7f
ndemeyvan <ndemeyvan@gmail.com>
2020-01-29 15:46:13 +0100

    envoi de message avec image

--- WerayouApp/app/src/main/java/com/example/werayouapp/Activity/ChatActivity.java
+++ WerayouApp/app/src/main/java/com/example/werayouapp/Activity/ChatActivity.java
@@ -7,6 +7,11 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
@@ -14,6 +19,7 @@ import android.view.WindowManager;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +27,7 @@ import com.example.werayouapp.R;
 import com.example.werayouapp.UtilsForChat.ChatAdapter;
 import com.example.werayouapp.UtilsForChat.DisplayAllChat;
 import com.example.werayouapp.UtilsForChat.ModelChat;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -32,8 +39,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -41,8 +54,10 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import id.zelory.compressor.Compressor;
 
 public class ChatActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -59,15 +74,16 @@ public class ChatActivity extends AppCompatActivity {
     //
     EditText editText;
     ImageButton sendButton;
+    ImageButton imageButton;
+    ImageView imageToSend;
     List<ModelChat> modelChatList;
     ChatAdapter chatAdapter;
     DatabaseReference reference;
-
-
-
-
-
-
+    StorageReference storageReference;
+    byte[] final_image;
+    Uri mImageUri;
+    private static  final int MAX_LENGTH =100;
+    private boolean isWithImage =false;
 
 
     @Override
@@ -81,7 +97,8 @@ public class ChatActivity extends AppCompatActivity {
         id_user=getIntent().getStringExtra("id");
         editText=findViewById(R.id.editText);
         sendButton=findViewById(R.id.sendButton);
-
+        imageButton=findViewById(R.id.imageButton);
+        imageToSend=findViewById(R.id.imageToSend);
         //
         user= FirebaseAuth.getInstance();
         userID=user.getCurrentUser().getUid();
@@ -112,15 +129,146 @@ public class ChatActivity extends AppCompatActivity {
             public void onClick(View view) {
                String msg= editText.getText().toString();
                if (!msg.isEmpty()){
-                   sendmessage(userID,id_user,msg);
+                   if (isWithImage==false){
+                       sendmessage(userID,id_user,msg);
+                   }else{
+                       sendMessageWithImage(userID,id_user);
+                       imageToSend.setVisibility(View.GONE);
+                       isWithImage=false;
+                   }
+
                }else{
-                    makeToast("entrer un message");
+                   makeToast("entrez un message");
                }
+            }
+        });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setImage();
             }
         });
 
         readMessage(userID,id_user);
     }
+
+
+
+    void setImage(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            try {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 555);
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(ChatActivity.this);
+
+            }catch (Exception e){
+                e.printStackTrace ();
+            }
+        } else {
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .start(ChatActivity.this);
+        }
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult ( requestCode, resultCode, data );
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                mImageUri = result.getUri();
+                File actualImage = new File(mImageUri.getPath());
+                try{
+
+                    Bitmap compressedImage = new Compressor(this)
+                            .setMaxWidth(250)
+                            .setMaxHeight(250)
+                            .setQuality(100)
+                            .compressToBitmap(actualImage);
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    compressedImage.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    final_image = baos.toByteArray();
+                    imageToSend.setImageURI(mImageUri);
+                    isWithImage=true;
+                    if (isWithImage==true){
+                        imageToSend.setVisibility(View.VISIBLE);
+                    }
+
+                }catch (Exception e){
+
+                }
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
+
+    void stockageWithURI(@NonNull Task<Uri> task,final String expediteur, final String recepteur){
+        Uri downloadUri;
+        if (task!=null){
+            downloadUri = task.getResult ();
+        }else{
+            downloadUri=mImageUri;
+        }
+        final DatabaseReference reference =FirebaseDatabase.getInstance ().getReference ();
+        Calendar calendar=Calendar.getInstance ();
+        SimpleDateFormat currentDate=new SimpleDateFormat (" dd MMM yyyy" );
+        String saveCurrentDate=currentDate.format ( calendar.getTime () );
+        String date=saveCurrentDate;
+        final HashMap<String,Object> messageMap = new HashMap<> (  );
+        messageMap.put ( "expediteur",expediteur );
+        messageMap.put ( "recepteur",recepteur );
+        messageMap.put ( "message",downloadUri.toString() );
+        messageMap.put ( "createdDate",date );
+        messageMap.put ( "type","image" );
+        reference.child ( "Chats" ).push ().setValue ( messageMap ).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                editText.setText("");
+            }
+        });
+        //
+        contact =new DisplayAllChat(  );
+        contact.setId_recepteur ( recepteur );
+        contact.setId_expediteur ( expediteur );
+        contact.setDernier_message ( "image" );
+        //
+        reference.child ( "dernier_message" )
+                .child(expediteur)
+                .child("contacts")
+                .child(recepteur).push ().setValue ( contact )
+                .addOnSuccessListener ( new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //
+                        contact =new DisplayAllChat (  );
+                        contact.setId_recepteur ( expediteur );
+                        contact.setId_expediteur ( recepteur );
+                        contact.setDernier_message ( "image" );
+                        //
+                        reference.child ( "dernier_message" )
+                                .child(recepteur)
+                                .child("contacts")
+                                .child(expediteur).push ().setValue ( contact ).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                            }
+                        });
+                    }
+                } );
+
+    }
+
+
+
 
     //recuperer les information de l'utilisateur
     public void getUserData(){
@@ -182,6 +330,43 @@ public class ChatActivity extends AppCompatActivity {
 
     }
 
+    void sendMessageWithImage(final String expediteur, final String recepteur){
+        //debut envoie dans storage
+        String random =random ();
+        final StorageReference ref = storageReference.child ( "messages_images" ).child ( random + " .jpg" );
+        UploadTask uploadTask = ref.putBytes(final_image);
+
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+                // Continue with the task to get the download URL
+                return ref.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+
+                    } else {
+                        // Handle failures
+                        // ...
+                    }
+                    stockageWithURI ( task,expediteur,recepteur);
+
+                } else {
+
+                    // Handle failures
+                    // ...
+                }
+            }
+        });
+    }
+
     public void sendmessage(final String expediteur, final String recepteur, final String message){
 
         final DatabaseReference reference =FirebaseDatabase.getInstance ().getReference ();
@@ -194,6 +379,7 @@ public class ChatActivity extends AppCompatActivity {
         messageMap.put ( "recepteur",recepteur );
         messageMap.put ( "message",message );
         messageMap.put ( "createdDate",date );
+        messageMap.put ( "type","message" );
         reference.child ( "Chats" ).push ().setValue ( messageMap ).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
@@ -275,4 +461,18 @@ public class ChatActivity extends AppCompatActivity {
         super.onBackPressed();
         finish();
     }
+
+
+    public static String random(){
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(MAX_LENGTH);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++){
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+
 }
