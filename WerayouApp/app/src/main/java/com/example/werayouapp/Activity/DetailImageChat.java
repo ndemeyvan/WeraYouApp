f826e8d4ef4908cc03a41a6a73e488f3e5c972a5
ndemeyvan <ndemeyvan@gmail.com>
2020-01-31 17:14:31 +0100

    travail sur les detail d'image dans le chat

--- WerayouApp/app/src/main/java/com/example/werayouapp/Activity/DetailImageChat.java
+++ WerayouApp/app/src/main/java/com/example/werayouapp/Activity/DetailImageChat.java
@@ -5,13 +5,21 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Magnifier;
 
 import com.bumptech.glide.Glide;
 import com.example.werayouapp.R;
@@ -25,6 +33,7 @@ public class DetailImageChat extends AppCompatActivity implements NavigationView
     Toolbar toolbar;
     FloatingActionButton floatingActionButton;
     private String imageLink;
+    private static final int code=1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,6 +88,19 @@ public class DetailImageChat extends AppCompatActivity implements NavigationView
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.image:
+                if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                    if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)==
+                            PackageManager.PERMISSION_DENIED){
+                        //la permission a ete refuse , redemande
+                        String[] permission ={Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        requestPermissions(permission,code);
+                    }else{
+                        //la permission a deja ete accorde
+                        donwload();
+                    }
+                }else{
+                    //le systeme est inferieur a android marshmallow
+                }
 
                 return true;
 
@@ -87,6 +109,34 @@ public class DetailImageChat extends AppCompatActivity implements NavigationView
         }
     }
 
+    void donwload(){
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(imageLink));
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI|
+                DownloadManager.Request.NETWORK_MOBILE);
+        request.setTitle("Download");
+        request.setDescription("telechargement en cour ...");
+        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS," " + System.currentTimeMillis());
+        DownloadManager manager= (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            switch (requestCode){
+                case code: {
+                    if (grantResults.length>0&&grantResults[0]==
+                    PackageManager.PERMISSION_GRANTED){
+                        donwload();
+                    }else{
+
+                    }
+                }
+            }
+    }
 
     @Override
     public void onBackPressed() {
