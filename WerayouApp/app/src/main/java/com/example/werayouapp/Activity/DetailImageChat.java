95f09cab18a2f2c08df303db00baaace1229c91b
ndemeyvan <ndemeyvan@gmail.com>
2020-01-31 16:42:29 +0100

    travail sur les detail d'image dans le chat

--- WerayouApp/app/src/main/java/com/example/werayouapp/Activity/DetailImageChat.java
+++ WerayouApp/app/src/main/java/com/example/werayouapp/Activity/DetailImageChat.java
@@ -1,17 +1,25 @@
 package com.example.werayouapp.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.example.werayouapp.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.squareup.picasso.Picasso;
 
-public class DetailImageChat extends AppCompatActivity {
+public class DetailImageChat extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
 
     ImageView imageView;
     Toolbar toolbar;
@@ -38,13 +46,47 @@ public class DetailImageChat extends AppCompatActivity {
             }
         });
 
-        Glide.with(this)
+        Picasso.with(this)
                 .load(imageLink)
                 .into(imageView);
 
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.download_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.image:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.image:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     @Override
     public void onBackPressed() {
